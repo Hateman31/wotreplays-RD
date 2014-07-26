88424eb9eb910af9d36ddfd3ccca9290eef8431e
Hateman31 <metilogrunge31@gmail.com>
2014-07-26 21:41:06 +0400

    Update UI.py

--- UI.py
+++ UI.py
@@ -2,13 +2,6 @@ from RD import *
 from tkinter import *
 from tkinter.ttk import Combobox
 
-root = Tk()
-root.title("Replays Downloader v 0.1")
-frame3 = Frame(root)
-frame4 = Frame(root)
-frame1 = Frame(frame3)
-frame2 = Frame(frame3)
-
 def Action():
     params={}
     params['filter'] = {}
@@ -33,35 +26,43 @@ def Action():
     else:
       SearchReplays(params)
 
-class Element(Frame):
-  def __init__(self,values,name,parent=None):
-    Frame.__init__(self,parent)
-    self.pack()
-    self.makeWidgets(values,name)
-  def makeWidgets(self,values,name):
-    self.combobox = Combobox(self,values=values,height=8)
-    self.labl = Label(self,text=name)
-    self.labl.grid(row=0,column=0)
-    self.combobox.grid(row=1,column=0)
+def Main(Action):
+	root = Tk()
+	root.title("Replays Downloader v 0.1")
+	frame3 = Frame(root)
+	frame4 = Frame(root)
+	frame1 = Frame(frame3)
+	frame2 = Frame(frame3)
+	class Element(Frame):
+	  def __init__(self,values,name,parent=None):
+		Frame.__init__(self,parent)
+		self.pack()
+		self.makeWidgets(values,name)
+	  def makeWidgets(self,values,name):
+		self.combobox = Combobox(self,values=values,height=8)
+		self.labl = Label(self,text=name)
+		self.labl.grid(row=0,column=0)
+		self.combobox.grid(row=1,column=0)
+	values1 = [x for x in tanks]
+	El1 = Element(sorted(values1),'Tank',frame1)
+	values2 = [x for x in maps]
+	El2 = Element(sorted(values2),'Map',frame1)
+	values3 = [x for x in types]
+	El3 = Element(sorted(values3),'Battle type',frame1)
+	lb1 = Label(frame2,text='Inflicted damage')
+	ent1=Entry(frame2)
+	lb2 = Label(frame2,text='Frags')
+	ent2=Entry(frame2)
+	lb3 = Label(frame2,text='Expirience')
+	ent3=Entry(frame2)
+	for x in (El1,El2,El3,lb1,ent1,lb2,ent2,lb3,ent3):
+		x.pack()
+	Button(frame4,text='Close',command=root.destroy).pack(side=RIGHT)
+	Button(frame4,text='Search', command=Action).pack(side=RIGHT)
+	frame1.grid(row=0,column=0)
+	frame2.grid(row=0,column=1)
+	frame3.grid(row=0,column=0)
+	frame4.grid(row=1,column=0)
+	root.mainloop()
 
-values1 = [x for x in tanks]
-El1 = Element(sorted(values1),'Tank',frame1)
-values2 = [x for x in maps]
-El2 = Element(sorted(values2),'Map',frame1)
-values3 = [x for x in types]
-El3 = Element(sorted(values3),'Battle type',frame1)
-lb1 = Label(frame2,text='Inflicted damage')
-ent1=Entry(frame2)
-lb2 = Label(frame2,text='Frags')
-ent2=Entry(frame2)
-lb3 = Label(frame2,text='Expirience')
-ent3=Entry(frame2)
-for x in (El1,El2,El3,lb1,ent1,lb2,ent2,lb3,ent3):
-    x.pack()
-Button(frame4,text='Close',command=root.destroy).pack(side=RIGHT)
-Button(frame4,text='Search', command=Action).pack(side=RIGHT)
-frame1.grid(row=0,column=0)
-frame2.grid(row=0,column=1)
-frame3.grid(row=0,column=0)
-frame4.grid(row=1,column=0)
-root.mainloop()
+Main(Action)
