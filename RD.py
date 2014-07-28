3e6893ee5f2c73b26d271f6ecabbc5ec8dbf594e
Hateman31 <metilogrunge31@gmail.com>
2014-07-28 10:29:01 +0400

    Update RD.py

--- RD.py
+++ RD.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup as bs
-#~ import requests
-from urllib.request import urlopen
-from urllib.request import urlretrieve
+from urllib.request import urlopen,urlretrieve
 import os
 from time import ctime
 
 #~ вынести в файл(закрытый от юзера) и при первом запуске создать его
-#~ при последующих - брать значения из файла(пока на сайте всё в порядке)(json???)
-mass = bs(urlopen('http://wotreplays.ru'))('ul')
-maps = {x.text.strip():x.find('input').get('map_id') for x in mass[7]('li')}
-tanks = {x.text.strip():x.find('input').get('tank_id') for x in mass[6]('li')}
-types = { x.text.strip(): x.find('input').get('battletype') for x in mass[9]('li')}
+#~ при последующих - брать значения из файла(пока на сайте всё в порядке)(sqlite? json?)
+def Contents():
+	mass = bs(urlopen('http://wotreplays.ru'))('ul')
+	def couples(ind,x):
+		return {x.text.strip():x.find('input').get(ind) for x in mass[x]('li')}
+	maps = couples('map_id',7)
+	tanks = couples('tank_id',6)
+	types = couples('battletype',9)
+
+#~ Tanks:
+	#~ ul = soup.find('ul',{'class':'b-list b-filter__list'})
+	#~ for li in ul('li'):
+		#~ li.find('input').get('tank_id') #id tank
+		#~ li.find('label').text #name of tank
 
 def Replay(record,params):
-	def trick(x):
-		return(x.text.strip())
+	trick = lambda x: x.text.strip()
 	replay = {}
 	buf = record('a')	#player
 	buf1 = record('li')		# damage,xp,frags
 	funcs = {'player':(lambda:buf[2].text),'damage':(lambda:trick(buf1[3])),
 			'frags':(lambda:trick(buf1[0])),'xp':(lambda:trick(buf1[1])),
 			'date':(lambda:record('span')[4].text)}
-	params = [p for p in params if p in list(funcs.keys())]
+	params = [p for p in params if p in funcs]
 	for p in params:
 		replay[p]=funcs[p]()
 	return(replay)
@@ -50,20 +56,19 @@ def Test(data, params):
 
 def GetUrl(params):
 	url ='http://wotreplays.ru/site/index'
-	args=params['filter']
-	for a in args:
-		url=''.join((url,'/',a,'/','%2C'.join(args[a].split(','))))
-	url = ''.join((url,'/sort'))
+	args = params['filter']
 	sort = params['sort']
-	choice = {}
-	if 'damage' in sort:
-			url = ''.join((url,'/inflicted_damage.desc'))
-	elif 'frags' in sort:
-			url = ''.join((url,'/frags.desc'))
-	elif 'xp' in sort:
-			url = ''.join((url,'/xp.desc'))
-	else:
-			url=''.join((url,'/upload_at.desc'))
+	for a in args:
+		substr = args[a].replace(',','%2C')
+		url='{1}/{2}/{3}/sort'.format(url,a,substr) # говнокод жи!!!!
+	choice = {
+			'damage':'/inflicted_damage.desc',
+			'frags':'/frags.desc',
+			'xp':'/xp.desc'
+			}
+	dfx = sorted([a for a in choice])
+	#~ for s in dfx:
+		#~ url+=s
 	return(url)
 
 def Load(link,name,path):
@@ -73,22 +78,21 @@ def Load(link,name,path):
 	urlretrieve(link, t_path)
 
 def SearchReplays(params,limit=25):
+	# это...
 	keys = []
 	for k in params:
 		for x in params[k]:
 			keys+=[x]
+	# ...ужасно ><
 	url = GetUrl(params)
-	path=''.join(('C:\\Users\\',os.environ.get('USERNAME'),'\\Desktop\\Downloaded Replays\\'))
+	path=''.join(('C:\\Users\\',os.environ.get('USERNAME'),'\\Desktop\\Downloaded Replays\\')) # это тоже ((
 	if not os.path.exists(path):
 		os.mkdir(path)
 	path=''.join((path,ctime().replace(':','-'),'\\'))
 	os.mkdir(path)
 	while limit:
-		# Use the Force!!!
-		# Seriously!
-		# Use the PyQuery
 		page = bs(urlopen(url))
-		for rec in [x for x in page('li')[-150:-20] if len(x)==13]:
+		for rec in page('div',class_='r-info')[:-1]:
 			rep = Replay(rec,keys)
 			if Test(rep,params['sort']):
 				link = rec('a')[3].get('href')
