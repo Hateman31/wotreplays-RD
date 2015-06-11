f37215b62b59d9fa402b131d03122fa1fc65ef07
hateman31 <metilogrunge31@gmail.com>
2015-06-11 21:01:38 +0300

    new feature

--- summ.py
+++ summ.py
@@ -1,11 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout	import BoxLayout
+import json
 
+with open('data') as f:
+	data = json.loads(f.read())
 class Foo(BoxLayout):
-	pass
+	tanks = [x['title'] for x in data['tank'][:5]]
+	maps = [x['title'] for x in data['map'][:5]]
+	battles = [x['title'] for x in data['battle_type'][:5]]
+	
 class SummApp(App):
 	def build(self):
 		return Foo()
 	
 if __name__ == '__main__':
 	SummApp().run()
+
