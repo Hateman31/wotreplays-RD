6f66c0df7ee249032bff448851d422bceb9176da
hateman31 <metilogrunge31@gmail.com>
2015-06-12 11:23:22 +0300

    full-length options

--- summ.py
+++ summ.py
@@ -5,9 +5,9 @@ import json
 with open('data') as f:
 	data = json.loads(f.read())
 class Foo(BoxLayout):
-	tanks = [x['title'] for x in data['tank'][:5]]
-	maps = [x['title'] for x in data['map'][:5]]
-	battles = [x['title'] for x in data['battle_type'][:5]]
+	tanks = [x['title'] for x in data['tank']]
+	maps = [x['title'] for x in data['map']]
+	battles = [x['title'] for x in data['battle_type']]
 	
 class SummApp(App):
 	def build(self):
