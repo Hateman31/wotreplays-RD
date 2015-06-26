aa45e7f9b1baa6a1cfe85ce896550ce47477e62a
hateman31 <metilogrunge31@gmail.com>
2015-06-26 20:29:22 +0300

    another commit #5

--- summ.py
+++ summ.py
@@ -4,21 +4,25 @@ import json
 
 with open('data1') as f:
 	data = json.loads(f.read())
-class Foo(BoxLayout):
+class Root(BoxLayout):
 	keys = ('tank','map','battle_type')
 	tanks,maps,battles = map(sorted,(data[x] for x in keys))
-	def action(self,tank,_map,battle):
+	def action(self,tank=None,_map=None,battle=None):
 		#~ продумать обработку комбинаций tank-map-battle
 		dlist = (tank,_map,battle)
-		q= 'wotreplays.ru/site/index/version/37/'
-		for key,rec in zip(self.keys,dlist):
-			#~ print(key,data[key][rec])
-			q+='{0}/{1}/'.format(key,data[key][rec])
-		print(q)
+		if all(dlist):
+			q= 'wotreplays.ru/site/index/version/37/'
+			for key,rec in zip(self.keys,dlist):
+				q+='{0}/{1}/'.format(key,data[key][rec])
+			print(q)
+		else:
+			print('Error! Some data is None!')
+class Folders(BoxLayout):
+	pass		
 class SummApp(App):
 	def build(self):
 		self.title = 'Replays Downloader'
-		return Foo()
+		return Root()
 	
 if __name__ == '__main__':
 	SummApp().run()
