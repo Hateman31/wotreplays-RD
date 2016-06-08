d55cf04cf41df5c99470cb3da5f941b92c8c7b58
hateman31 <metilogrunge31@gmail.com>
2016-06-08 10:40:34 +0300

    another

--- ReplaysDownloader.py
+++ ReplaysDownloader.py
@@ -4,13 +4,20 @@ import json
 import os
 import wget
 
+replayObject = dict.fromkeys(self.keys)
+
+def valueFromText(x): 
+	css = 'i[class*="%s"]'
+	text = html.select(css % x)[0].parent.text
+	return int(text.strip())
+	
 class ReplaysDownloader:
 
 	def __init__(self,url,params):
 		self.keys = ('dmg','xp','frags')
 		self.params = params
 		self.targets = None
-		self.takeAllReplays()
+		self.html_to_replays()
 	
 	def checkValue(self,replay,field):
 		return replay[field]>=self.params[field]
@@ -19,28 +26,34 @@ class ReplaysDownloader:
 		return all(checkValue(replay,field) for field in self.keys)
 	
 	def findTargets(self):
-		params = self.stuff['params']	
 		for replay in self.replays:
 			if self.replayIsGood(replay):
 				self.addNewTarget(replay['url'])
 	
-	def takeAllReplays(self):
+	def html_to_replays(self):
 		replays_html = self.site.select('div.r-info')[:-1]
 		for replay_div in replays_html:
-			self.replays +=[replayObject(replay_div)]
+			self.add_replay_object(replay_div)
 
+	def add_replay_object(self,html):
+		for key in self.keys:
+			replayObject[key] = valueFromText(key)
+		replayObject['url'] = html.find('a').get('href')
+		self.replays += [replayObject]
+	
 	def crawling(self):
 		self.site = Site(self.url)
+		#TODO: site.notLastPage() to property
 		#while self.site.NotLastPage:
 		while self.site.notLastPage():
-			self.takeAllReplays()
-			self.linx = self.findLinks()
+			self.html_to_replays()
+			self.findTargets()
 
 	def addNewTarget(self,url):
 		pass
 
-	def Get_URL_and_name(url):
-		buf = url.split(#)
+	def get_URL_and_name(url):
+		buf = url.split('#')
 		buf[0] = buf[0].replace('/site/','site/download/')
-		return [buf[0],'name':buf[-1]]
+		return [buf[0],buf[-1]]
 	
--- spider.py
+++ spider.py
@@ -2,17 +2,3 @@ import os
 import requests as r
 from bs4 import BeautifulSoup as bs
 from site import Site
-
-#вынести в класс Replay
-def replayObject(html):
-	'''Take params of replay from page'''
-	css = 'i[class*="%s"]'
-	res = dict.fromkeys(['frags','xp','dmg'])
-	
-	getText = lambda x : html.select(css % x)[0].parent.text
-	
-	for x in res:
-		res[x] = int(getText(x).strip())		
-	res['url'] = html.find('a').get('href')
-	
-	return res
